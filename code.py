9a339bbad5b2987cb4d75cead2b248277193806c
SS COMPUTER <jahidjesan508@gmail.com>
2023-06-20 00:08:06 +0600

    new project creat

--- code.py
+++ code.py
@@ -3,3 +3,6 @@ import math
 # input two value and min-max int(type data)
 lower = int(input("Enter Lower Bound Value:"))
 upper = int(input("Enter Upper Bound Value:"))
+# Generating random number lower-upper case
+x = random.randint(lower,upper)   # output change fixing numbers
+print()
